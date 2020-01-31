4f49c3130ff77cd4c4d91ff135d8b6a0c32ea2a0
BuildTools <26531652+Kas-tle@users.noreply.github.com>
2020-01-30 23:47:15 -0800

    Update var.js

--- var.js
+++ var.js
@@ -1,4 +1,5 @@
 var plugin_data = {
+
 	id: 'discord-rpc',
 	title: 'Discord Rich Presence',
 	icon: 'announcement', //Material icon name
@@ -6,4 +7,21 @@ var plugin_data = {
 	description: 'Show a rich presence status in Discord',
 	version: '1.0.0', //Plugin version
 	variant: 'desktop'	// 'both', 'web', 'desktop'
-}
+};
+
+(function() {
+
+var setting;
+
+Plugin.register("discord-rpc", {
+"author": "strajabot & Kastle",
+"icon": "announcement",
+"version": "1.0.0",
+"description": "Show a rich presence status in Discord",
+onload() {
+
+		setting = new Setting('obfiscaterpc', {
+			value: true,
+			name: 'Discord Rich Prescense',
+			description: 'Obfiscate Project Name',
+		})
