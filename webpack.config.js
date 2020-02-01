1546956f3b044e7e82218566a60d92fd833cce80
BuildTools <26531652+Kas-tle@users.noreply.github.com>
2020-01-31 20:41:47 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,8 @@
 const path = require('path')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 const BUILD_DIR = path.resolve(__dirname, 'dist/')
 
 const config = {
-  optimization: {
-    minimizer: [
-      new UglifyJSPlugin({
-        uglifyOptions: {
-          output: {
-            comments: false,
-            beautify: true,
-          },
-        },
-      }),
-    ],
-  },
   entry: {
     plugin: './main.js'
   },
